1961e04badb0ef0f163271b57f7dfec0ea5f1f67
Jason Diaz <jason@Jasons-MacBook-Air.local>
2022-01-26 18:36:34 -0700

    add code

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your Code Here
+
+let playagain = true
+
+function play() {
+	let userName = window.prompt('what is your name: ')
+	let score = 0
+
+	for (let i = 0; i < questions.length; i++) {
+		let question = questions[i]
+		let userAnswer = window.prompt(question.text)
+
+		if (userAnswer === question.correctAnswer) {
+			console.log("correct")
+			score += 10
+		} else {
+			console.log("incorrect")
+		}
+	}
+
+	window.prompt("Your score is " + score)
+}
+
+while (playagain === true) {
+	play()
+	let choice = window.prompt("wish to play again?")
+
+	if (choice == "Yes" || choice == "yes") {
+		playagain = true
+	} else {
+		playagain = false
+		window.alert("Thanks for playing")
+	}
+}
